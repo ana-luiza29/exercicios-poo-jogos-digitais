2472a8a98a1c313bf90467e5e23014b36d0ea8bb
Jgmor4es <freitas.analuiza29@gmail.com>
2025-04-14 09:48:19 -0400

    add finish exercicio 5

---
+++ exercicio5/Modelo/Personagem.cs
@@ -0,0 +1,34 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace exercicio5.Modelo
+{
+public class Personagem
+    {
+        public string Nome;
+        public int Mana;
+
+        public Personagem(string nome, int mana)
+        {
+            Nome = nome;
+            Mana = mana;
+        }
+
+        public void UsarHabilidade(Habilidade habilidade)
+        {
+            if (Mana >= habilidade.CustoDeMana)
+            {
+                Mana -= habilidade.CustoDeMana;
+                Console.WriteLine($"{Nome} usou {habilidade.Nome}! (Mana restante: {Mana})");
+                habilidade.Executar();
+            }
+            else
+            {
+                Console.WriteLine($"{Nome} não tem mana suficiente para usar {habilidade.Nome}!");
+            }
+        }
+    }
+}
