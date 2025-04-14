62759ca3905cf50f8de37034f501dfe6236810a2
Jgmor4es <freitas.analuiza29@gmail.com>
2025-04-14 10:15:02 -0400

    add finish exercicio 15

---
+++ exercicio15/Modelo/EstadoPatrulha.cs
@@ -0,0 +1,21 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace exercicio15.Modelo
+{
+public class EstadoPatrulha : IEstado
+    {
+        public void Executar(Inimigo inimigo)
+        {
+            Console.WriteLine($"{inimigo.Nome} está patrulhando...");
+
+            if (inimigo.JogadorVisivel)
+            {
+                inimigo.TrocarEstado(new EstadoPerseguindo());
+            }
+        }
+    }
+}
---
+++ exercicio15/Modelo/IEstado.cs
@@ -0,0 +1,13 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace exercicio15.Modelo
+{
+    public interface IEstado
+    {
+         void Executar(Inimigo inimigo);
+    }
+}
