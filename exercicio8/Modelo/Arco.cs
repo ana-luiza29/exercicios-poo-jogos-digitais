097a2b556447c47725896c22edafccdbea6c9002
Jgmor4es <freitas.analuiza29@gmail.com>
2025-04-14 09:58:34 -0400

    add finish exercicio 8

---
+++ exercicio8/Modelo/Arco.cs
@@ -0,0 +1,14 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace exercicio8.Modelo
+{
+public class Arco : IArma
+    {
+        public string Nome => "Arco Elétrico";
+        public int AtaqueBonus => 7;
+    }
+}
---
+++ exercicio8/Modelo/IArma.cs
@@ -0,0 +1,14 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace exercicio8.Modelo
+{
+    public interface IArma
+    {
+         string Nome { get; }
+    int AtaqueBonus { get; }
+    }
+}
